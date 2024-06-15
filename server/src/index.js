5a921dd562687943519fda0fb8cbb0811430d23d
Basu Kumar <basutulsyan710@gmail.com>
2024-06-15 16:42:30 +0530

    add pre-installation

--- server/src/index.js
+++ server/src/index.js
@@ -7,6 +7,7 @@ import userModel from "./models/user.model.js";
 import recipeModel from "./models/recipe.model.js";
 import userRouter from "./routes/user.route.js";
 import recipesRouter from "./routes/recipe.route.js";
+import path from "path";
 
 app.use(express.json());
 app.use(cors());
@@ -14,6 +15,13 @@ dotenv.config();
 
 app.use("/auth", userRouter);
 app.use("/recipes", recipesRouter);
+
+const _dirname = path.resolve();
+
+app.use(express.static(path.join(_dirname, "/client/build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(_dirname, "/client/build/index.html"));
+});
 
 mongoose
   .connect(process.env.mongo_connection)
